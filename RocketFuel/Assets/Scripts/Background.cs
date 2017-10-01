ba1d43fe0f921723a2a83d4f691847adca70c232
LoucaColes <coleslouca@yahoo.co.uk>
2017-10-01 17:32:39 +0100

    Scrolling Background
    Just a Test scrolling background we could use if we are going with the
    escape from the center of the earth theme. will modify so its a bit more
    random.

---
+++ RocketFuel/Assets/Scripts/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float m_offset;
+    public float m_delay;
+
+    private GameObject m_lastObject = null;
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Background" && other.gameObject != m_lastObject)
+        {
+            GameObject t_gameobject = other.gameObject;
+            m_lastObject = t_gameobject;
+            StartCoroutine(MoveBackground(t_gameobject));
+        }
+    }
+
+    private IEnumerator MoveBackground(GameObject _background)
+    {
+        yield return new WaitForSeconds(m_delay);
+        print(_background.name);
+        print("position: " + _background.transform.position);
+        _background.transform.position += new Vector3(0f, m_offset, 0f);
+        print("new position: " + _background.transform.position);
+    }
+}
