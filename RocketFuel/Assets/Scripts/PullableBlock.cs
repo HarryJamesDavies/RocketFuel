ab0dae7c018e2e0c1829278efde3a7b3601c7547
LoucaColes <coleslouca@yahoo.co.uk>
2017-10-03 17:38:31 +0100

    RumbleSoundEffect
    RumbleSoundEffect

--- RocketFuel/Assets/Scripts/PullableBlock.cs
+++ RocketFuel/Assets/Scripts/PullableBlock.cs
@@ -32,6 +32,9 @@ public class PullableBlock : MonoBehaviour
 
     private Vector3 m_targetPosition;
 
+    private AudioSource m_audioSource;
+    public AudioClip m_sfx;
+
     //private SpriteRenderer m_spriteRenderer;
 
     //public Sprite[] m_sprites;
@@ -39,6 +42,7 @@ public class PullableBlock : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
+        m_audioSource = GetComponent<AudioSource>();
         m_clonePrefab = gameObject;
         m_minAngle = -m_maxAngle;
         m_lerpTime = 0f;
@@ -78,6 +82,7 @@ public class PullableBlock : MonoBehaviour
     {
         if (!m_clone)
         {
+            m_audioSource.PlayOneShot(m_sfx, 1.0f);
             Vector3 t_position = transform.position;
             m_targetPosition = GetNewPosition(t_position);
             m_clone = (GameObject)Instantiate(m_clonePrefab, transform.position, Quaternion.identity);
