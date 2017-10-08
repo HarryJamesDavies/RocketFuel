a627a6b5808c8d336c4a95371c01598416f5cde1
LoucaColes <coleslouca@yahoo.co.uk>
2017-10-08 15:24:10 +0100

    Power Ups, Modified Camera, Player Movement
    Added Power Ups, although currently dont spawn.
    Modified the camera to work a bit better but will improve on it more.
    Added my version of player movement, need to discuss as a group which
    one is better and what improvements can be made.

---
+++ RocketFuel/Assets/Scripts/LoucaMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class LoucaMovement : MonoBehaviour
+{
+    private Rigidbody2D m_rb2D;
+
+    private Player m_player;
+
+    public float m_moveSpeed;
+    public float m_jumpHeight;
+    public float m_fallMultiplier;
+    public float m_rayDist;
+    private float m_horMovement;
+
+    private Vector2 m_velocity;
+
+    private bool m_jumping;
+
+    private LayerMask m_layerMask;
+
+    public int m_maxJumps;
+    private int m_jumpCount;
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_rb2D = GetComponent<Rigidbody2D>();
+        m_player = ReInput.players.GetPlayer(0);
+        m_jumping = false;
+        m_layerMask = 1;
+        m_jumpCount = m_maxJumps;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_velocity = Vector2.zero;
+        m_horMovement = m_player.GetAxis("Move Horizontal") * m_moveSpeed * Time.deltaTime;
+        m_velocity = new Vector2(m_horMovement, 0f);
+
+        if (m_player.GetButtonDown("Jump"))
+        {
+            RaycastHit2D t_hit = Physics2D.Raycast(transform.position, Vector2.down, m_rayDist, m_layerMask);
+            if (t_hit)
+            {
+                print(t_hit.collider.tag);
+                if (t_hit.collider.tag == "Ground" || t_hit.collider.tag == "Manip")
+                {
+                    m_jumping = true;
+                    m_jumpCount--;
+                }
+            }
+            if (!t_hit && m_jumpCount > 0)
+            {
+                m_jumping = true;
+                m_jumpCount--;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_jumping)
+        {
+            m_jumping = false;
+            m_velocity.y = m_jumpHeight;
+        }
+        m_rb2D.velocity += m_velocity;
+        if (m_rb2D.velocity.y == 0f)
+        {
+            if (m_jumpCount != m_maxJumps)
+            {
+                RaycastHit2D t_hit = Physics2D.Raycast(transform.position, Vector2.down, m_rayDist, m_layerMask);
+                if (t_hit)
+                {
+                    print(t_hit.collider.tag);
+                    if (t_hit.collider.tag == "Ground" || t_hit.collider.tag == "Manip")
+                    {
+                        m_jumpCount = m_maxJumps;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Power" && other.GetComponent<PowerUp>())
+        {
+            PowerUp t_powerUpScript = other.GetComponent<PowerUp>();
+            switch (t_powerUpScript.GetPowerType())
+            {
+                case PowerUp.PowerType.TripleJump:
+                    m_jumpCount = m_maxJumps++;
+                    break;
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
